ffe1ac6f420bf00757b0e2603c5e8c1f06612656
Kae Benton <sonja.k.benton@gmail.com>
2020-08-12 15:47:31 -0400

    folders and files added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React { useState, useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import Images from './Photos/Images'
+import Text from './Text/Text'
+
+
 
 function App() {
+  const [] = useState()
+  
+  useEffect(() => {
+
+  });
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   return (
     <div className="App">
+    <Header />
+    <Footer />
+    <Images />
+    <Text />
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
