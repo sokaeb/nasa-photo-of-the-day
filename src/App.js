af3c16b2447e83c6ecaf065074f026b9508a21ad
Kae Benton <sonja.k.benton@gmail.com>
2020-08-13 18:40:34 -0400

    Added styles to all components.

--- src/App.js
+++ src/App.js
@@ -4,74 +4,86 @@ import "./App.css";
 import Header from './Components/Header';
 import Body from './Components/Body'
 import Footer from './Components/Footer';
-import {BASE_URL, API_KEY} from './Constants'
+import {BASE_URL, API_KEY} from './Constants';
 
 
 export default function App() {
-  const [imgOfDay, setImgOfDay] = useState(null)
-  const [explaination, setExplaination] = useState('')
-  const [title, setTitle] = useState('')
-  const [date, setDate] = useState('')
-  const [copyright, setCopyright] = useState('')
+  const [nasaData, setNasaData ] = useState({})
+  // const [imgOfDay, setImgOfDay] = useState(null)
+  // const [explaination, setExplaination] = useState('')
+  // const [title, setTitle] = useState('')
+  // const [date, setDate] = useState('')
+  // const [copyright, setCopyright] = useState('')
  
-// setting title on HEADER 
-useEffect(() => {
-  axios.get(`${BASE_URL}?api_key=${API_KEY}`)
-    .then(res => {
-      setTitle(res.data.title)
-    })
-    .catch(err => {
-      debugger
-    })
-}, [])
-// setting date on HEADER
-useEffect(() => {
-  axios.get(`${BASE_URL}?api_key=${API_KEY}`)
-    .then(res => {
-      setDate(res.data.date)
-    })
-    .catch(err => {
-      debugger
-    })
-}, [])
-// setting img of day on BODY
   useEffect(() => {
     axios.get(`${BASE_URL}?api_key=${API_KEY}`)
-    .then(res => {
-      setImgOfDay(res.data.url)
-    })
-    .catch(err => {
-      debugger
-    })
-  }, []);
-// setting explanation on BODY
-  useEffect(() => {
-    axios.get(`${BASE_URL}?api_key=${API_KEY}`)
-    .then(res => {
-      setExplaination(res.data.explanation)
-      console.log(res.data.explanation)
-    })
-    .catch(err => {
-      debugger
-    })
-  }, []);
-// setting copyright on FOOTER
-useEffect(() => {
-  axios.get(`${BASE_URL}?api_key=${API_KEY}`)
-    .then(res => {
-      setCopyright(res.data.copyright)
-    })
-    .catch(err => {
-      debugger
-    })
-}, [])
+      .then(res => {
+        setNasaData(res.data)
+      })
+      .catch(err => {
+        debugger
+      })
+  }, [])
+
+  
+// // setting title on HEADER 
+// useEffect(() => {
+//   axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+//     .then(res => {
+//       setTitle(res.data.title)
+//     })
+//     .catch(err => {
+//       debugger
+//     })
+// }, [])
+// // setting date on HEADER
+// useEffect(() => {
+//   axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+//     .then(res => {
+//       setDate(res.data.date)
+//     })
+//     .catch(err => {
+//       debugger
+//     })
+// }, [])
+// // setting img of day on BODY
+//   useEffect(() => {
+//     axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+//     .then(res => {
+//       setImgOfDay(res.data.url)
+//     })
+//     .catch(err => {
+//       debugger
+//     })
+//   }, []);
+// // setting explanation on BODY
+//   useEffect(() => {
+//     axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+//     .then(res => {
+//       setExplaination(res.data.explanation)
+//       console.log(res.data.explanation)
+//     })
+//     .catch(err => {
+//       debugger
+//     })
+//   }, []);
+// // setting copyright on FOOTER
+// useEffect(() => {
+//   axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+//     .then(res => {
+//       setCopyright(res.data.copyright)
+//     })
+//     .catch(err => {
+//       debugger
+//     })
+// }, [])
 
   
   return (
     <div className="App">
-      <Header title={title} date={date} />
-      <Body imgOfDay={imgOfDay} explain={explaination} />
-      <Footer copyright={copyright} />
+      <Header nasaData={nasaData} />
+      <Body nasaData={nasaData} />
+      <Footer nasaData={nasaData} />
       {/* <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,31 +1,41 @@
 import React from 'react';
-import axios from 'axios';
 import styled from 'styled-components';
 
+
 const StyledBody = styled.div`
-/* max-width: 100%; */
-border: 2px solid palevioletred;
+/* border: 2px solid palevioletred; */
 display: flex;
 flex-direction: column;
 align-items: center;
+color: ${props => props.theme.secondaryColor};
+
+
 img {
-    width: 60%;
-    border: 2px solid #7a1f5c; 
+    width: 70%;
+    /* border: 2px solid #9D219D; */
+    margin: 5% 0 5%; 
 };
 
 p {
-    color: blue;
+    margin: 0 5% 5%;
+    padding-top: 2%;
+    padding-bottom: 2%;
+    width: 70%;
+    line-height: 1.5;
+    text-align: justify;
+    border-top: 2px dashed black;
+    border-bottom: 2px dashed black;
 };
 
 `
 
 export default function Body(props) {
-    const {imgOfDay, explain} = props;
+    const {nasaData} = props;
     
     return (
         <StyledBody className="Body">
-            <img src={imgOfDay} alt="NASA Astronomy of the Day"/>
-            <p>{explain}</p>
+            <img src={nasaData.url} alt="NASA Astronomy of the Day"/>
+            <p>{nasaData.explanation}</p>
         </StyledBody>
     );
   }
